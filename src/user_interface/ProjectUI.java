807a92aa9ef3b1a9c2756503fc500c009ed3d581
haanmiba <haanmiba@gmail.com>
2017-11-04 13:29:58 -0400

    Added initial files

---
+++ src/user_interface/ProjectUI.java
@@ -0,0 +1,15 @@
+package user_interface;
+
+import javax.swing.*;
+
+public class ProjectUI implements Runnable {
+
+	private JFrame frame;
+	
+	public void run() {
+		
+		frame = new JFrame("Project");
+		
+	}
+
+}
