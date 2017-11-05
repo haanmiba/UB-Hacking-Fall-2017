9670b7ff5cd61f2e4bbc7a39d1369047cd2d99ed
haanmiba <haanmiba@gmail.com>
2017-11-04 21:41:45 -0400

    Color spectrum with buffered image

--- src/user_interface/MainWindow.java
+++ src/user_interface/MainWindow.java
@@ -2,13 +2,17 @@ package user_interface;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import model.Model;
 
 public class MainWindow implements Runnable {
 
+	
 	private JFrame frame;
 	private int appWidth;
 	private int appHeight;
@@ -22,20 +26,25 @@ public class MainWindow implements Runnable {
 		setAppHeightAndWidth();
 		
 		BufferedImage image = createColorSpectrum();
+		ImageIcon i = new ImageIcon(image);
 		
 		_model = new Model();
 		_model.setUI(this);
 		frame = new JFrame("Project");
 		mainPanel = new JPanel();
-		JLabel testLabel = new JLabel("My Label");
+		JLabel testLabel = new JLabel();
+		testLabel.setIcon(i);
 		
 		/*
 		mainPanel.add(testLabel);
 		frame.add(mainPanel);*/
 		
-		ColorSpectrumPanel csp = new ColorSpectrumPanel(image);
+		//ColorSpectrumPanel csp = new ColorSpectrumPanel(image);
 		
-		frame.getContentPane().add(new JLabel(new ImageIcon(image)));
+		//frame.getContentPane().add(new JLabel(new ImageIcon("src/images/color-spectrum.jpg")));
+		
+		mainPanel.add(testLabel);
+		frame.add(mainPanel);
 		
 		//DO NOT TOUCH
 		frame.setSize(appWidth, appHeight);
@@ -51,11 +60,13 @@ public class MainWindow implements Runnable {
 	
 	public BufferedImage createColorSpectrum() {
 		BufferedImage image = new BufferedImage(appWidth, appHeight, BufferedImage.TYPE_INT_ARGB);
-		for (int x = 0; x < appWidth; x++) {
-			for (int y = 0; y < appHeight; y++) {
-				image.setRGB(x, y, 200);
+		
+		for (int x = 0; x < appWidth/2; x++) {
+			for (int y = 0; y < appHeight/2; y++) {
+				image.setRGB(x, y, (new Color(x % 256, y % 256, 0).getRGB()));
 			}
 		}
+		
 		return image;
 	}
 	
